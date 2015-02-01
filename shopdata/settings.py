02c7f08c93bce47aea68b21da07b9d2e017b554a
Sonny Kwok <sonny.kwok@capitalone.com>
2015-02-01 15:07:49 -0500

    added settings_local.py

--- shopdata/settings.py
+++ shopdata/settings.py
@@ -81,3 +81,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Amazon API credential
+CLIENT_ID = None
+CLIENT_SECRET = None
+
+try:
+    from local_settings import *
+except ImportError as e:
+    pass
