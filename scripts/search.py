4ea5db6f85605b2d8ab38f6780185e20246a7e0f
Sonny Kwok <sonny.kwok@capitalone.com>
2015-02-02 00:26:20 -0500

    updated script to print images

--- scripts/search.py
+++ scripts/search.py
@@ -10,6 +10,14 @@ config = {
 
 api = amazonproduct.API(cfg=config)
 
-items = api.item_search('Books', Publisher="O'Reilly")
-for book in items:
-	print '%s: "%s"' % (book.ItemAttributes.Author, book.ItemAttributes.Title)
+# items = api.item_search('Books', Publisher="O'Reilly")
+# for book in items:
+# 	print '%s: "%s"' % (book.ItemAttributes.Author, book.ItemAttributes.Title)
+
+result = api.item_lookup('B006H3MIV8', ResponseGroup='Images')
+for item in result.Items.Item:
+	print item.ASIN
+	if item.LargeImage:
+		print '%s - %s x %s' % (item.LargeImage.URL,
+								item.LargeImage.Width,
+								item.LargeImage.Height)
