894c891cfd317e800b2ddfcfa5c9455e1002b04a
Sonny Kwok <sonny.kwok@capitalone.com>
2015-02-17 09:40:36 -0500

    corrected BrowseView

--- shopdata/catalog/views.py
+++ shopdata/catalog/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import View
-from shopdata.catalog.views import Catalog
+from shopdata.catalog.models import Catalog
 
 # Create your views here.
-class Browse(View):
+class BrowseView(View):
 
-	def get(self, request):
-		age = request.GET.get('age', default="21+")
-		gender = request.GET.get('gender', default="boy")
-        catalog = Catalog.objects.get(age=age, gender=gender)
-        data = {}
-        return render(request, 'overview.html', data)
+	def get(self, request, gender='boy', age='21+'):
+		catalog = Catalog.objects.get(age=age, gender=gender)
+		data = {}
+		return render(request, 'overview.html', data)
 
 	def lookup_imgs(self, amazon_node_id):
 		pass
