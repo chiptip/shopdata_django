0ff2b80dcfcf6aeb707d530929f9b280c1c2a408
Sonny Kwok <sonny.kwok@capitalone.com>
2015-02-17 09:40:56 -0500

    added admin view to Catalog / Product

--- shopdata/catalog/admin.py
+++ shopdata/catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from shopdata.catalog.models import Catalog, Product
 # Register your models here.
+
+
+class CatalogAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Catalog, CatalogAdmin)
+admin.site.register(Product, ProductAdmin)
